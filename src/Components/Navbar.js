01785a2fb5368163b487ab5f0707951321e5da52
Aleksandar <coa@hotmail.com>
2023-01-30 14:43:12 +0100

    upload

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import shopicon from "../Data/shopIcon.png"
 
 export const Navbar = () => {
     return (
@@ -6,7 +7,7 @@ export const Navbar = () => {
                <div class="nav-wrapper">
                   <div class="grad-bar"></div>
                   <nav class="navbar">
-                    {/* ICONHERE */}
+                    <img src={shopicon}></img>
                      {/* <img src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c8/Bluestar_%28bus_company%29_logo.svg/1280px-Bluestar_%28bus_company%29_logo.svg.png" alt="Company Logo"> */}
                      <div class="menu-toggle" id="mobile-menu">
                         <span class="bar"></span>
